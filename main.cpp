789d6fafb2c014524d3808cebb5be813ef73f221
axeldinh <axeldvc@gmail.com>
2021-12-01 16:35:47 +0100

    Add Mother class for solvers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <Eigen/Dense>
+
+//#include "GeneralEigenSolver.h"
+#include "src/foo.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    foo A;
+    A.print();
+    //auto solver = new GeneralEigenSolver<double>();
+    //auto m = Eigen::Matrix<double, 3, 3>::Ones();
+    //solver->setMatrix(m);
+    //auto m_c = solver->getMatrix();
+    //std::cout << m_c << std::endl;
     return 0;
 }
---
+++ src/GeneralEigenSolver.h
@@ -0,0 +1,14 @@
+//
+// Created by axeld on 01/12/2021.
+//
+
+#ifndef EIGENVALUE_PROJECT_GENERALEIGENSOLVER_H
+#define EIGENVALUE_PROJECT_GENERALEIGENSOLVER_H
+
+
+class GeneralEigenSolver {
+
+};
+
+
+#endif //EIGENVALUE_PROJECT_GENERALEIGENSOLVER_H
