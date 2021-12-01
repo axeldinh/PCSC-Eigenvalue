f53cc1a1b20675e7fa7083fc3c1f387b984fa9b7
axeldinh <axeldvc@gmail.com>
2021-12-01 17:49:32 +0100

    Setters for Matrix and EigenVector, add TODOs

--- src/GeneralEigenSolver.h
+++ src/GeneralEigenSolver.h
@@ -7,13 +7,13 @@
  * All methods are implemented in the header file as the compiler needs the code when calling on
  * a particular template (e.g GeneralEigenSolver<double>)
  *
- * TODO Make the class pure virtual
  */
 
 #ifndef EIGENVALUE_PROJECT_GENERALEIGENSOLVER_H
 #define EIGENVALUE_PROJECT_GENERALEIGENSOLVER_H
 
 #include <Eigen/Dense>
+#include <cassert>
 
 template<typename ScalarType>
 using MatrixType = Eigen::Matrix<ScalarType, Eigen::Dynamic, Eigen::Dynamic>;
@@ -24,6 +24,7 @@ using VectorType = Eigen::Vector<ScalarType, Eigen::Dynamic>;
 template<typename ScalarType>
 class GeneralEigenSolver {
 private:
+    // TODO make mMatrix a pointer to a given matrix
     MatrixType<ScalarType> mMatrix;
     VectorType<ScalarType> mEigenVector;
     double mThreshold;
@@ -36,39 +37,77 @@ public:
     ~GeneralEigenSolver();
 
     // Setters
+    void setMatrix(MatrixType<ScalarType> A);
+    void setEigenVector(VectorType<ScalarType> V);
     void setThreshold(double threshold);
     void setMaxIter(int maxIter);
 
     // Getters
-    double getThreshold();
-    int getMaxIter();
+    double getThreshold() const;
+    int getMaxIter() const;
+
+    // TODO add a pure virtual solve method
+    virtual ScalarType solve(bool logging = false) = 0;
 };
+
+/********************************************//**
+ *  Constructors and Destructors
+ ***********************************************/
 
 template<typename ScalarType>
 GeneralEigenSolver<ScalarType>::GeneralEigenSolver() {
-    mMatrix.fill(1.);
+    mEigenVector.setRandom();
+    mThreshold = 1e-15;
+    mMaxIter = 1000;
 }
 
 template<typename ScalarType>
-GeneralEigenSolver<ScalarType>::~GeneralEigenSolver() {}
+GeneralEigenSolver<ScalarType>::~GeneralEigenSolver() {
+
+    // TODO investigate the destructor
+}
+
+/********************************************//**
+ *  Setters
+ ***********************************************/
 
 template<typename ScalarType>
-void GeneralEigenSolver<ScalarType>::setThreshold(double threshold) {
+void GeneralEigenSolver<ScalarType>::setMatrix(const MatrixType<ScalarType> A) {
+    mMatrix.resize(A.rows(), A.cols());
+    mMatrix = A;
+}
+
+template<typename ScalarType>
+void GeneralEigenSolver<ScalarType>::setEigenVector(const VectorType<ScalarType> V) {
+    mEigenVector.resize(V.rows(), V.cols());
+    mEigenVector = V;
+}
+
+template<typename ScalarType>
+void GeneralEigenSolver<ScalarType>::setThreshold(const double threshold) {
+    // TODO add exceptions (>0 and not too small) ValueErrorException
+    assert(threshold > 0);
     mThreshold = threshold;
 }
 
 template<typename ScalarType>
-void GeneralEigenSolver<ScalarType>::setMaxIter(int maxIter) {
+void GeneralEigenSolver<ScalarType>::setMaxIter(const int maxIter) {
+    // TODO add exceptions
+    assert(maxIter > 0);
     mMaxIter = maxIter;
 }
 
+/********************************************//**
+ *  Getters
+ ***********************************************/
+
 template<typename ScalarType>
-double GeneralEigenSolver<ScalarType>::getThreshold() {
+double GeneralEigenSolver<ScalarType>::getThreshold() const {
     return mThreshold;
 }
 
 template<typename ScalarType>
-int GeneralEigenSolver<ScalarType>::getMaxIter() {
+int GeneralEigenSolver<ScalarType>::getMaxIter() const{
     return mMaxIter;
 }
 
